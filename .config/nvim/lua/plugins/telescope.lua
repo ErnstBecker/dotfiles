79945782fc5c1a310036ed6a819918e301c8ca20
Ernst Becker <ernst.becker.neto@gmail.com>
2025-10-23 18:47:56 -0300

    feat(nvim): add keybind for show hidden files on telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -17,6 +17,9 @@ return {
       vim.keymap.set('n', '<C-p>', builtin.find_files, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
       vim.keymap.set('n', '<leader><leader>', builtin.oldfiles, {})
+			vim.keymap.set('n', '<C-h>', function()	
+				require('telescope.builtin').find_files({ hidden = true }) 
+			end, {})
 
       require('telescope').load_extension('ui-select')
     end,
