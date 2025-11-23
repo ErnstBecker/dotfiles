d85f89d7dc24754a3c7b850252e336bc5ca83c2b
Ernst Becker <ernst.becker.neto@gmail.com>
2025-11-23 16:35:44 -0300

    feat(zen): add new ui and privacy configs

--- .zen/ernst.default/user.js
+++ .zen/ernst.default/user.js
@@ -6,6 +6,7 @@ user_pref("browser.tabs.allow_transparent_browser", true);
 user_pref("zen.theme.content-element-separation", 0);
 user_pref("zen.view.experimental-no-window-controls", true);
 user_pref("apz.overscroll.enabled", false);
+user_pref("browser.tabs.inTitlebar", 0);
 
 // Tabs
 user_pref("zen.tabs.show-newtab-vertical", false);
@@ -16,10 +17,10 @@ user_pref("zen.pinned-tab-manager.restore-pinned-tabs-to-pinned-url", true);
 // Enable userChrome.css
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
-
 // Dark Theme
 user_pref("extensions.activeThemeID", "firefox-compact-dark@mozilla.org");
 user_pref("layout.css.prefers-color-scheme.content-override", 0);
+user_pref("ui.systemUsesDarkTheme", 1);
 
 
 // Address Bar
@@ -35,18 +36,23 @@ user_pref("browser.urlbar.suggest.trending", false);
 user_pref("browser.urlbar.suggest.history", false);
 user_pref("browser.urlbar.suggest.engines", false);
 
+// Search
+user_pref("browser.search.defaultenginename", "SearXNG")
+user_pref("browser.search.separatePrivateDefault", false)
 
 // Bookmarks
 user_pref("browser.toolbars.bookmarks.visibility", "never");
 user_pref("browser.bookmarks.restore_default_bookmarks", false);
 
 
-// Extensions
-// Disable extensions/themes recommendations
-user_pref("extensions.htmlaboutaddons.recommendations.enabled", false)
-user_pref("extensions.recommendations.themeRecommendationUrl", false);
-user_pref("extensions.getAddons.showPane", false);
-user_pref("extensions.getAddons.discovery.api_url", "");
+// Security
+user_pref("permissions.default.desktop-notification", 2);
+user_pref("dom.security.https_only_mode", true)
+
+
+// Enable DevTools
+user_pref("devtools.chrome.enabled", true)
+user_pref("devtools.debugger.remote-enabled", true);
 
 
 // Bloatware
@@ -71,3 +77,10 @@ user_pref("network.cookie.cookieBehavior", 1);
 // Disable Experimental Features
 user_pref("browser.preferences.experimental", false);
 user_pref("browser.preferences.experimental.hidden", true);
+
+// Extensions
+// Disable extensions/themes recommendations
+user_pref("extensions.htmlaboutaddons.recommendations.enabled", false)
+user_pref("extensions.recommendations.themeRecommendationUrl", false);
+user_pref("extensions.getAddons.showPane", false);
+user_pref("extensions.getAddons.discovery.api_url", "");
