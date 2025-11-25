0c378d0a17ec8931ecc4e6a110945189903a1ec3
Ernst Becker <ernst.becker.neto@gmail.com>
2025-11-25 07:12:54 -0300

    feat(nvim): add vscode diff to neovim

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,6 +1,14 @@
 return {
-	'lewis6991/gitsigns.nvim',
-	opts = {
-		current_line_blame = true
+	{
+		'lewis6991/gitsigns.nvim',
+		opts = {
+			current_line_blame = true
+		}
+	},
+	{
+		{
+			"esmuellert/vscode-diff.nvim",
+			dependencies = { "MunifTanjim/nui.nvim" },
+		}
 	}
 }
