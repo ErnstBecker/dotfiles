c82ce9e7e11428a013490943053f33c543698130
Ernst Becker <ernst.becker.neto@gmail.com>
2025-12-02 15:34:47 -0300

    chore(nvim): change telescope default up/down keybind

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -13,6 +13,16 @@ return {
 						"%.git/",
 						"%.vscode/",
 					},
+					mappings = {
+						n = {
+							["j"] = require("telescope.actions").move_selection_next,
+							["k"] = require("telescope.actions").move_selection_previous,
+						},
+						i = {
+							["<C-j>"] = require("telescope.actions").move_selection_next,
+							["<C-k>"] = require("telescope.actions").move_selection_previous,
+						},
+					},
 				},
 				pickers = {
 					find_files = {
