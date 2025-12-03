ed228e055e7ae4d2add6e686d24bedec9e4787a4
Ernst Becker <ernst.becker.neto@gmail.com>
2025-12-03 14:16:34 -0300

    fix(nvim): add lazy loading to color scheme plugins and update transparency groups

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -1,6 +1,6 @@
 return {
 	{ "rebelot/kanagawa.nvim", lazy = false },
-	{ "datsfilipe/vesper.nvim" },
-	{ "morhetz/gruvbox" },
+	{ "datsfilipe/vesper.nvim", lazy = false },
+	{ "morhetz/gruvbox", lazy = false },
 	{ "catppuccin/nvim", name = "catppuccin", lazy = false },
 }
--- .config/nvim/lua/plugins/transparency.lua
+++ .config/nvim/lua/plugins/transparency.lua
@@ -2,11 +2,11 @@ return {
 	"xiyaowong/transparent.nvim",
 	opts = {
 		groups = {
-			'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
-			'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
-			'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
-			'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
-			'EndOfBuffer',
+			"Normal", "NormalNC", "Comment", "Constant", "Special", "Identifier",
+			"Statement", "PreProc", "Type", "Underlined", "Todo", "String", "Function",
+			"Conditional", "Repeat", "Operator", "Structure", "LineNr", "NonText",
+			"SignColumn", "CursorLine", "CursorLineNr", "StatusLine", "StatusLineNC",
+			"EndOfBuffer", "NormalFloat"
 		},
 		extra_groups = {
 			-- Git
