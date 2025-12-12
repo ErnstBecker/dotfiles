f8cd0cfe8d4f52446e76c8a5cc1dfb30f17f2388
Ernst Becker <81073546+ErnstBecker@users.noreply.github.com>
2025-12-12 02:38:05 -0300

    chore: update lualine configuration sections

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -4,6 +4,20 @@ return {
 		require("lualine").setup({
 			options = {
 				theme = "auto"
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff" },
+				lualine_c = { "filename" },
+
+				lualine_x = {
+					"diagnostics",
+					"fileformat",
+					"filetype",
+				},
+
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
 			}
 		})
 	end
