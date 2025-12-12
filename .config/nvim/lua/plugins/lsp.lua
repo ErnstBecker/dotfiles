3888cb7689dddb9c8f7d2ecf7e816889862e6fc6
Ernst Becker <81073546+ErnstBecker@users.noreply.github.com>
2025-12-12 02:38:14 -0300

    chore: remove lua-language-server command from lsp configuration

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -31,7 +31,6 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			vim.lsp.config.lua_ls = {
-				cmd = { "lua-language-server" },
 				settings = {
 					Lua = {
 						diagnostics = {
