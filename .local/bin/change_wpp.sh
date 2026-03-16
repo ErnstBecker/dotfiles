c48a616140eca7558e3d9e2058ccb312eddece3c
Ernst Becker <81073546+ErnstBecker@users.noreply.github.com>
2026-03-16 15:11:41 -0300

    chore: update swww to awww

--- .local/bin/change_wpp.sh
+++ .local/bin/change_wpp.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 if [ -z "$1" ]; then
-    echo "Use: $0 <file>"
-    exit 1
+	echo "Use: $0 <file>"
+	exit 1
 fi
 
 cp "$1" ~/.config/background
 
-swww img --transition-fps=180 --transition-type=grow --transition-pos=center --transition-duration=1 ~/.config/background
+awww img --transition-fps=180 --transition-type=grow --transition-pos=center --transition-duration=1 ~/.config/background
