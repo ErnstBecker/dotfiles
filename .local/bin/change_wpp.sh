865e43dab480571c68f1f1496464ffa11362dd1f
Ernst Becker <81073546+ErnstBecker@users.noreply.github.com>
2026-03-31 15:57:44 -0300

    chore: add a verification for awww-daemon

--- .local/bin/change_wpp.sh
+++ .local/bin/change_wpp.sh
@@ -7,4 +7,6 @@ fi
 
 cp "$1" ~/.config/background
 
-awww img --transition-fps=180 --transition-type=grow --transition-pos=center --transition-duration=1 ~/.config/background
+if pgrep -x awww-daemon > /dev/null 2>&1; then
+	awww img --transition-fps=180 --transition-type=grow --transition-pos=center --transition-duration=1 ~/.config/background
+fi
