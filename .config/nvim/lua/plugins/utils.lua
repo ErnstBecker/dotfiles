f8c52d392678899ebe849079cb217fcb2870934a
Ernst Becker <81073546+ErnstBecker@users.noreply.github.com>
2026-06-05 14:54:19 -0300

    feat(nvim): add nvim-colorizer plugin

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -1,9 +1,14 @@
 vim.pack.add ({
 	"https://github.com/tpope/vim-surround",
 	"https://github.com/windwp/nvim-autopairs",
+	"https://github.com/catgoose/nvim-colorizer.lua",
 })
 
 require("nvim-autopairs").setup({
 	event = "InsertEnter",
 	config = true
+})
+
+require("colorizer").setup({
+	filetypes = { "*" },
 })
