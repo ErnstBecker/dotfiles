56dc4dab37fe29faca9b65b7c0e551e71bf09840
Ernst Becker <81073546+ErnstBecker@users.noreply.github.com>
2026-08-29 11:42:15 -0300

    feat(rofi): generate scaled background image for input bar

--- .local/bin/change_wpp.sh
+++ .local/bin/change_wpp.sh
@@ -7,6 +7,8 @@ fi
 
 cp "$1" ~/.config/background
 
+ffmpeg -y -loglevel error -i "$1" -vf "scale=800:-1" ~/.cache/rofi_background.png
+
 if pgrep -x awww-daemon > /dev/null 2>&1; then
 	awww img --transition-fps=180 --transition-type=grow --transition-pos=center --transition-duration=1 ~/.config/background
 fi
